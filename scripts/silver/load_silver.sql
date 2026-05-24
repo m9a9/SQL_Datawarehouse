a620c93da5253a933cad29952234061bbd807829
Mahmoud Ahmed <47575298+m9a9@users.noreply.github.com>
2026-05-24 11:07:19 +0300

    Create stored procedure to load data into Silver layer
    This stored procedure loads cleaned data from the Bronze layer into the Silver layer, including customer information, product details, sales data, and ERP data. It truncates existing tables before inserting the new data.

---
+++ scripts/silver/load_silver.sql
@@ -0,0 +1,196 @@
+--!  == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == = 
+--!  Stored Procedure: Load Silver Layer (Bronze -> Silver)
+--!  == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+create or alter procedure silver.load_silver as
+begin 
+
+--TODO:: cleaned data crm_cust_info and inserting to silver layer
+truncate table silver.crm_cust_info;
+
+insert into
+    silver.crm_cust_info(
+        cst_id,
+        cst_key,
+        cst_firstname,
+        cst_lastname,
+        cst_marital_status,
+        cst_gndr,
+        cst_create_date
+    )
+select
+    cst_id,
+    cst_key,
+    trim(cst_firstname) as cst_firstname,
+    TRIM(cst_lastname) as cst_lastname,
+    case
+        when UPPER(cst_marital_status) = 'S' then 'SINGLE'
+        when UPPER(cst_marital_status) = 'M' then 'Married'
+        else 'N/A'
+    end cst_marital_status,
+    case
+        when UPPER(cst_gndr) = 'M' then 'Male'
+        when UPPER(cst_gndr) = 'F' then 'Female'
+        else 'N/A'
+    end cst_gndr_desc,
+    cst_create_date
+from
+    (
+        select
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY cst_id
+                ORDER BY
+                    cst_create_date
+            ) AS flag_last
+        from
+            bronze.crm_cust_info
+        where
+            cst_id is NOT NULL
+    ) T
+where
+    flag_last = 1 --! TODO:: cleaned data crm_prd_info and inserting to silver layer
+    truncate table silver.crm_prd_info;
+
+insert into
+    silver.crm_prd_info(
+        prd_id,
+        cat_id,
+        prd_key,
+        prd_nm,
+        prd_cost,
+        prd_line,
+        prd_start_dt,
+        prd_end_dt
+    )
+select
+    prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS CAT_ID,
+    SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_KEY,
+    prd_nm,
+    isnull(prd_cost, 0) as prd_cost,
+    case
+        upper(trim(prd_line))
+        when 'R' then 'Road'
+        when 'M' then 'Mountain'
+        when 'T' then 'Touring'
+        when 'S' then 'other sales'
+        else 'N/A'
+    end as prd_line,
+    cast(prd_start_dt as date) as prd_start_dt,
+    --! the end date must be higher than the start date,so we use lead function to avoid overlapping of the date range, 
+    --! and should minus 1 day to make sure the end date is higher than the start date
+    cast(
+        lead(prd_start_dt) over(
+            partition by prd_key
+            order by
+                prd_start_dt
+        ) -1 as date
+    ) as prd_end_dt
+from
+    bronze.crm_prd_info -- !TODO:: cleaned data crm_sales_info and inserting to silver layer
+    truncate table silver.crm_sales_details;
+
+INSERT INTO
+    silver.crm_sales_details(
+        sls_ord_num,
+        sls_prd_key,
+        sls_cust_id,
+        sls_order_dt,
+        sls_ship_dt,
+        sls_due_dt,
+        sls_sales,
+        sls_quantity,
+        sls_price
+    )
+select
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    case
+        when sls_order_dt = 0
+        or len(sls_order_dt) != 8 then null
+        else cast(cast(sls_order_dt as varchar) as date)
+    end as sls_order_dt,
+    case
+        when sls_ship_dt = 0
+        or len(sls_ship_dt) != 8 then null
+        else cast(cast(sls_ship_dt as varchar) as date)
+    end as sls_ship_dt,
+    case
+        when sls_due_dt = 0
+        or len(sls_due_dt) != 8 then null
+        else cast(cast(sls_due_dt as varchar) as date)
+    end as sls_due_dt,
+    case
+        when sls_sales <= 0
+        or sls_sales is null
+        or sls_sales != sls_quantity * abs(sls_price) then sls_quantity * abs(sls_price)
+        else sls_sales
+    end as sls_sales,
+    sls_quantity,
+    case
+        when sls_price <= 0
+        or sls_price is null then sls_sales / nullif(sls_quantity, 0)
+        else sls_price
+    end sls_price
+from
+    bronze.crm_sales_details -- !TODO:: CLEAN ERP_DATA and inserting to silver layer
+    truncate table silver.erp_loc_a101;
+
+insert into
+    silver.erp_loc_a101(cid, cntry)
+select
+    replace (cid, '-', '') as cid,
+    case
+        when trim(cntry) = 'DE' then 'Germany'
+        when trim(cntry) in ('US', 'USA') then 'United states'
+        when trim(cntry) = ' '
+        or cntry is null then 'n/a'
+        else trim(cntry)
+    end as country
+from
+    bronze.erp_loc_a101 --!TODO :: clean erp_cust_az12 and inserting to silver layer
+    truncate table silver.erp_cust_az12;
+
+insert into
+    silver.erp_cust_az12(
+        cid,
+        bdate,
+        gen
+    )
+select
+    case
+        when cid like 'NAS%' then substring(cid, 4, len(cid))
+        else cid
+    end as cid,
+    case
+        when bdate > getdate() then null
+        else bdate
+    end as bdate,
+    case
+        when upper(trim(gen)) in ('M', 'MALE') THEN 'Male'
+        when upper(trim(gen)) in ('F', 'FEMALE') THEN 'Female'
+        else 'N/A'
+    end as gen
+from
+    bronze.erp_cust_az12 -- !TODO:: clean data erp_px_cat_g1v2 and inserting to silver layer
+    TRUNCATE TABLE silver.erp_px_cat_g1v2;
+
+PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
+
+INSERT INTO
+    silver.erp_px_cat_g1v2 (
+        id,
+        cat,
+        subcat,
+        maintenance
+    )
+SELECT
+    id,
+    cat,
+    subcat,
+    maintenance
+FROM
+    bronze.erp_px_cat_g1v2;
+end
